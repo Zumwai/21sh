68a7fd7aa0498c35822d31fbd35284e07af414b9
anna <pianistka08@mail.ru>
2021-03-08 14:29:10 +0300

    change in parsing

--- srcs/tokens/parse_tokens.c
+++ srcs/tokens/parse_tokens.c
@@ -22,6 +22,47 @@ static char 				*get_data(char *line, int *n, t_flag *flag)
 	char			*buf;
 
 	j = 0;
+	while (line[*n] != '\0') {
+		while (line[*n] != '|' && line[*n] != ';' && line[*n] != '&' && line[*n] != '>' &&
+			   line[*n] != '<' && line[*n] != '\0') {
+			update_flag(flag, line[*n]);
+			res[j] = line[*n];
+			*n += 1;
+			j += 1;
+		}
+		res[j] = line[*n];
+		if (semantica(flag, line, n, &j) == 0)
+			continue;
+		if (semantica(flag, line, n, &j) == 1 && j != 0) {
+			res[j] = '\0';
+			buf = ft_strdup(res);
+			ret = ft_strtrim(buf);
+			free(buf);
+			return (ret);
+		}
+		if (j == 0) {
+			ret = get_semantica_ret(line, n, res, j);
+			*n += 1;
+			return (ret);
+		}
+		else
+		{
+			res[j] = line[*n];
+			*n += 1;
+			j += 1;
+		}
+	}
+	/*}*/
+	buf = ft_strdup(res);
+	ret = ft_strtrim(buf);
+	free(buf);
+	return (ret);
+	/*char 			res[1000];
+	int 			j;
+	char			*ret;
+	char			*buf;
+
+	j = 0;
 	while (line[*n] != '\0')
 	{
 		while (line[*n] != '|' && line[*n] != ';' && line[*n] != '&' && line[*n] != '>' &&
@@ -38,7 +79,7 @@ static char 				*get_data(char *line, int *n, t_flag *flag)
 				res[j] = line[*n];
 			*n += 1;
 			j += 1;*/
-            res[j] = line[*n];
+           /* res[j] = line[*n];
             *n += 1;
             j += 1;
 		}
@@ -67,7 +108,7 @@ static char 				*get_data(char *line, int *n, t_flag *flag)
 	buf = ft_strdup(res);
 	ret = ft_strtrim(buf);
 	free(buf);
-	return (ret);
+	return (ret);*/
 }
 
 int						is_empty(char *s)
