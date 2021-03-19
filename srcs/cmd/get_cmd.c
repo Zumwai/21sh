22b687205d783665229e0a8cb70f414ea8c9c7cd
anna <pianistka08@mail.ru>
2021-03-19 20:04:37 +0300

    f

--- srcs/cmd/get_cmd.c
+++ srcs/cmd/get_cmd.c
@@ -54,33 +54,40 @@ int						len_of_word(char *s, int i)
 	char				c;
 
 	res = 0;
-	///printf("len s == %s i == %d\n", s, i);
+	printf("len s == %s i == %d\n", s, i);
 	while (s[i] && s[i] != ' ')
 	{
 		if (s[i] != ' ' && s[i] != 34 && s[i] != 39)
 		{
-			///printf("s[%d] === %c\n", i, s[i]);
+			printf("s[%d] === %c\n", i, s[i]);
 			i++;
 			res++;
 		}
-		if (s[i] == 34 || s[i] == 39)
+		if (s[i] == '"' || s[i] == '\'')
 		{
 			c = s[i];
+			printf("c === %c\n", c);
 			i++;
-			while (s[i] != c && s[i])
-			{
-				if (s[i] == c)
-					break ;
-				///printf("s[%d] === %c\n", i, s[i]);
+			while (s[i] != c && s[i]) {
+				printf("sss[%d] === %c\n", i, s[i]);
 				i++;
 				res++;
 			}
-			///res--;
 		}
+		if (s[i] == c)
+		{
+			i++;
+			break;
+		}
+		///res--;
+
 		if (s[i] == ' ')
-			break ;
+		{
+			i++;
+			break;
+		}
 	}
-	///printf("len_of_word === %d\n", res);
+	printf("len_of_word === %d\n", res);
 	return (res);
 }
 
@@ -111,25 +118,38 @@ char					*fill_str(char *s, int *i)
 	int					l;
 	char				buf[1000];
 	int 				course;
-
+	char 				c;
 
 	j = 0;
+	course = 0;
 	course = (*i);
-	///printf("start course === %d\n", course);
-	///printf("st.course === %c\n", s[course]);
+
+	ft_putendl(course + 48);
+	ft_putendl("BEWARE");
+
+	printf("start course === %d\n", course);
+	printf("st.course === %c\n", s[course]);
 	if (s[course] == ' ')
 	{
 		while (s[course] == ' ' && s[course])
 			course++;
 	}
-	///printf("s === %s\n", s);
+	printf("s === %s\n", s);
 	///printf("course === %d\n", course);
 	l = len_of_word(s, course);
 	res = NULL;
+	if (s[course] == '"' || s[course] == 39)
+	{
+		c = s[course];
+		course = course + 1;
+	}
 	while (j < l)
 	{
-		if (s[course] == '"' || s[course] == 39)
+		if (s[course] == c)
+		{
 			course = course + 1;
+			continue ;
+		}
 		buf[j] = s[course];
 		j++;
 		course = course + 1;
@@ -137,10 +157,10 @@ char					*fill_str(char *s, int *i)
 	///*i = *i + 1;
 	buf[j] = '\0';
 	///if (its_redir(buf))
-		///fill_str(s, &i);
+	///fill_str(s, &i);
 	///else
-		res = ft_strdup(buf);
-		///printf("res == %s\n", res);
+	res = ft_strdup(buf);
+	printf("res == %s\n", res);
 	(*i) = course;
 	return(res);
 }
@@ -166,67 +186,69 @@ int						how_much_restreams(char *s)
 	return (res);
 }
 
- int					how_much_words(char *s)
- {
- 	int					i;
- 	int					res;
- 	char				c;
+int					how_much_words(char *s)
+{
+	int					i;
+	int					res;
+	char				c;
 
- 	i = 0;
- 	res = 0;
- 	c = 0;
- 	while (s[i])
+	i = 0;
+	res = 0;
+	c = 0;
+	while (s[i])
 	{
- 		if (s[i] == 34 || s[i] == 39)
+		if (s[i] == 34 || s[i] == 39)
 		{
- 			c = s[i];
- 			i++;
- 			while (s[i] && s[i] != c)
- 			{
+			c = s[i];
+			i++;
+			while (s[i] && s[i] != c)
+			{
 				i++;
 			}
- 			i = i + 1;
+			i = i + 1;
 		}
- 		if (s[i] != '\0' && s[i] != ' ')
+		if (s[i] != '\0' && s[i] != ' ')
 		{
- 			while (s[i] != '\0' && s[i] != ' ')
- 			i++;
+			while (s[i] != '\0' && s[i] != ' ')
+				i++;
 		}
- 		if (s[i] == ' ')
+		if (s[i] == ' ')
 		{
- 			while (s[i] == ' ')
- 				i++;
+			while (s[i] == ' ')
+				i++;
 		}
- 		res++;
+		res++;
 	}
- 	return (res);
- }
+	printf("how_much_words === %d\n", res);
+	return (res);
+}
 
- char					**s_to_arr(char *s)
- {
- 	int					i;
- 	char				**res;
- 	int					j;
- 	int					c;
- 	int 				f;
- 	int					g;
+char					**s_to_arr(char *s)
+{
+	int					i;
+	char				**res;
+	int					j;
+	int					c;
+	int 				f;
+	int					g;
 
- 	c = 0;
- 	j = 0;
- 	f = how_much_words(s);
- 	g = how_much_restreams(s);
- 	i = f - g;
- 	res = (char **)malloc(sizeof(char *) * i + 1);
- 	while (j < f)
+	c = 0;
+	j = 0;
+	f = how_much_words(s);
+	g = how_much_restreams(s);
+	i = f - g;
+	printf("%d === words\n", i);
+	res = (char **)malloc(sizeof(char *) * (i + 1));
+	while (j <= i - 1)
 	{
- 		res[j] = fill_str(s, &c);
- 		j++;
- 		///printf("s_to_arr c === %d\n", c);
- 		c = c + 1;
+		res[j] = fill_str(s, &c);
+		j++;
+		///printf("s_to_arr c === %d\n", c);
+		c = c + 1;
 	}
- 	res[j] = '\0';
- 	return (res);
- }
+	res[j] = '\0';
+	return (res);
+}
 
 char					get_spec(char s)
 {
@@ -291,12 +313,12 @@ static t_cmd			*get_data_cmd(t_token *t, t_cmd *c, t_env **env)
 			get_env_val(buf, &j, t->data, &i, env);
 			i++;
 		}
-		if (t->data[i] && t->data[i] != 92 && t->data[i] != 39 && t->data[i] != 34)
+		if (t->data[i] && t->data[i] != 92 /*&& t->data[i] != 39 && t->data[i] != 34*/)
 			buf[j++] = t->data[i++];
 	}
 	buf[j] = '\0';
 	src = ft_strdup(buf);
-	///printf("src === %s\n", src);
+	printf("src === %s\n", src);
 	c->arr = s_to_arr(src);
 	free(src);
 	return (c);
